4fbf21206669f5911a5a198826390d570f112345
chomieu <chomieu@gmail.com>
2020-12-17 17:51:47 -0800

    Formatted script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
     var today = moment().format("dddd, MMMM Do")
     var time = parseInt(moment().format("H"))
     const hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
-    
+
     // Displays current date
     $("#currentDay").text(today)
-    
+
     // Generates time-blocks with <textarea> for description of the task and a save button
     // If the task has previously been defined (or changed) and saved, shows the saved task 
     for (i = 0; i < hours.length; i++) {
@@ -17,9 +17,9 @@ $(document).ready(function () {
         $("#" + i).append($("<button>", { class: "col-1 saveBtn fa fa-save" }))
 
         // Colorizes each time-block based on its time v.s. the current time by giving it a class
-        if (time < i+9) {
+        if (time < i + 9) {
             $("#" + i).addClass("future")
-        } else if (time === i+9) {
+        } else if (time === i + 9) {
             $("#" + i).addClass("present")
         } else {
             $("#" + i).addClass("past")
@@ -27,7 +27,7 @@ $(document).ready(function () {
     }
 
     // Saves the task to local storage when user clicks on the button
-    $("button").on("click", function(event) {
+    $("button").on("click", function (event) {
         event.preventDefault()
         var savedID = $(this).parent().attr("id")
         var task = $(this).prev().val()
